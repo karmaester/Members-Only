a85890c13f96a2afc2dc8b2b0cddbc58298188df
Khristian Rojas <khristianrojas15@gmail.com>
2020-11-27 18:38:34 -0300

    Delete edit, show, destroy and update mathods and restrict actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    helper_method :display_button_new  
+  helper_method :display_button_log
+
+
+    def display_button_log(current_user)
+        if current_user.present?
+          return "<a href=#{destroy_user_session_path} data-method='delete' rel='nofollow'> Log out </a>".html_safe
+        end
+    
+        "<a href=#{new_user_session_path}> Log in </a>".html_safe
+      end
+    
+      def display_button_new(current_user)
+        if current_user.present?
+          return "<a class='button is-success' href='/posts/new'>New Post</a>".html_safe
+        end
+      end
+    
 end
